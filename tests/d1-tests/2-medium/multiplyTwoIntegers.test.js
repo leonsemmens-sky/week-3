a4950c79a67687cc7d4da8f22a39391b32124a2d
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-10-17 16:28:29 +0100

    Update multiplyTwoIntegers.test.js

--- tests/d1-tests/2-medium/multiplyTwoIntegers.test.js
+++ tests/d1-tests/2-medium/multiplyTwoIntegers.test.js
@@ -6,7 +6,7 @@ const { multiplyTwoIntegers } = require('../../../d1-tests/2-medium/multiplyTwoI
 // checks if multiplyTwoIntegers multiplies 0 and 3 and returns 0
 // checks if multiplyTwoIntegers multiplies 5 and -6 and returns -30
 // checks if multiplyTwoIntegers multiplies -6 and -4 and returns 24
-// if multiplyTwoIntegers is passed a value other than a number, it should return "Inputs must be a number"
+// if multiplyTwoIntegers is passed a value other than a number, it should return "Inputs must be a number."
 // if multiplyTwoIntegers is passed a decimal value, it should return "Integers only."
 
 describe('test the multiplyTwoIntegers function', () => {
