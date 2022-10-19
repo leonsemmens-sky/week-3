1a3823c6584755799db3004fab7cf1f04a04e861
Jordan <43437534+Jaardy@users.noreply.github.com>
2022-10-19 13:52:39 +0100

    removed solution on Character

--- d3-static/1-easy/static.js
+++ d3-static/1-easy/static.js
@@ -1,14 +1,4 @@
-class Character {
-    constructor (name) {
-        this.name = name
-        this.xp = 1
-        this.hp = 100
-        this.mana = 50
-        Character.totalCharacters++
-    }
 
-    static totalCharacters = 0
-}
 
 
 
